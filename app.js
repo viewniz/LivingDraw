0808695dbb65401e5755cd6874ffbdcede38a1de
SehyeonGil <rlftpgus@naver.com>
2019-07-01 23:07:11 +0900

    7.1 test

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var goodsRouter = require('./routes/goods');
+var borderRouter = require('./routes/border');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/goods', goodsRouter);
 app.use('/users', usersRouter);
+app.use('/border', borderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
