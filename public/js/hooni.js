bd6b844c0e8690131a535f66127d79c9dcc290dd
hoonihu1 <hoonihu@gmail.com>
2019-07-08 23:27:01 +0900

    front_07_08

--- public/js/hooni.js
+++ public/js/hooni.js
@@ -1,5 +1,36 @@
 jQuery(document).ready(function(){
-    jQuery('#paintstory').on('click', function(event) {        
-        jQuery('#paintdetail').toggle('show');
-    });
+	jQuery('#paintstory').on('click', function(event) {        
+		jQuery('#paintdetail').toggle('show');
+	});
+
+	$('#leftside_detail_1').on({
+		'click': function(){
+			var image = $('#select_image_1').attr('src');
+			$('#leftside_detail_1').attr('class','leftside_detail');
+			$( '#leftside_detail_2').attr('class','leftside_normal');
+			$( '#leftside_detail_3').attr('class','leftside_normal');
+			$('#select_image').attr('src',image);
+		}
+	});
+
+	$('#leftside_detail_2').on({
+		'click': function(){
+			var image = $('#select_image_2').attr('src');
+			$('#leftside_detail_2').attr('class','leftside_detail');
+			$('#leftside_detail_1').attr('class','leftside_normal');
+			$('#leftside_detail_3').attr('class','leftside_normal');
+			$('#select_image').attr('src',image);
+		}
+	});
+
+	$('#leftside_detail_3').on({
+		'click': function(){
+			var image = $('#select_image_3').attr('src');
+			$('#leftside_detail_3').attr('class','leftside_detail');
+			$( '#leftside_detail_2').attr('class','leftside_normal');
+			$( '#leftside_detail_1').attr('class','leftside_normal');
+			$('#select_image').attr('src',image);
+		}
+	});
+
 });
