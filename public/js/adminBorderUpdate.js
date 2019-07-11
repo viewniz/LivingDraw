d9a3249c2cfa890eb8ed6f82e5de53c17b99a86b
SehyeonGil <rlftpgus@naver.com>
2019-07-11 17:46:01 +0900

    Admin Update V1

--- public/js/adminBorderUpdate.js
+++ public/js/adminBorderUpdate.js
@@ -1,16 +1,30 @@
 function readURL(input) {
-
     if (input.files && input.files[0]) {
         var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $(this).next().next().attr('src', e.target.result);
-        };
-
+        reader.onload = function(e) {
+            $(input).next().children('img').attr('src', e.target.result);
+            $(input).next().css('display','inline-block');
+        }
         reader.readAsDataURL(input.files[0]);
     }
 }
 
-$(".imgInput").change(function(){
+$("input[type='file']").change(function() {
     readURL(this);
 });
+
+function removeImage(val,val2){
+    let item = {num:val,id:val2};
+    jQuery.ajaxSettings.traditional=true;
+    $.ajax({
+        method: "POST",
+        type: "POST",
+        url: "/admin/border/update/removeImage",
+        data: item,
+        success: function (data) {
+            if (data === "clear") {
+                location.reload();
+            }
+        }
+    });
+}
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -24,4 +24,6 @@ router.get('/border/update/:id', controller.admin_border_update);
 router.post('/border/upload', uploadPicture.array('pic'), controller.admin_border_upload_post);
 router.post('/border/delete', controller.admin_border_delete_post);
 router.post('/border/isSellingChange', controller.admin_border_is_selling_change);
+router.post('/border/update/removeImage', controller.admin_border_update_remove_image);
+router.post('/border/update', uploadPicture.array('pic'), controller.admin_border_update_post);
 module.exports = router;
