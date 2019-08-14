bd7758d67af4bab84358a246328f7917ad0878f7
hoonihu1 <hoonihu@gmail.com>
2019-08-14 17:54:30 +0900

    login_Social_front

--- public/js/author_register.js
+++ public/js/author_register.js
@@ -4,4 +4,26 @@ jQuery(document).ready(function () {
     }, function () {
         $('#hidden_next').css("transform", "translateX(100%)");
     });
+<<<<<<< HEAD
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#image_section').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#imgInput").change(function () {
+        readURL(this);
+        $(".image_border").css('height', 'auto');
+    });
+
+
+=======
+>>>>>>> 327bb1d4b0dc963bec63cc2e887306900d22114e
 });
