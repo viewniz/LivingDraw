1e5648b9b41420c94bb929f3952b8a30bc6480ef
hoonihu1 <hoonihu@gmail.com>
2019-08-14 22:17:04 +0900

    190814_front

--- public/js/author_register.js
+++ public/js/author_register.js
@@ -1,19 +1,32 @@
 jQuery(document).ready(function () {
+
     $(".next").hover(function () {
         $('#hidden_next').css("transform", "translateX(0%)");
     }, function () {
         $('#hidden_next').css("transform", "translateX(100%)");
     });
-<<<<<<< HEAD
+
+    $(".pre_next").hover(function () {
+        $('#hidden_pre_next').css("transform", "translateX(0%)");
+    }, function () {
+        $('#hidden_pre_next').css("transform", "translateX(-100%)");
+    });
+
+
+    $(function () {
+        $('#btn-upload').click(function (e) {
+            e.preventDefault();
+            $('#imgInput').click();
+
+        });
+    });
 
     function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
-
             reader.onload = function (e) {
                 $('#image_section').attr('src', e.target.result);
             }
-
             reader.readAsDataURL(input.files[0]);
         }
     }
@@ -22,8 +35,4 @@ jQuery(document).ready(function () {
         readURL(this);
         $(".image_border").css('height', 'auto');
     });
-
-
-=======
->>>>>>> 327bb1d4b0dc963bec63cc2e887306900d22114e
 });
