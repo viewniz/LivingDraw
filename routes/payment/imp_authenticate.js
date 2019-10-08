f5d6e3d80f3acb2426ac2c5310c59858bd337f31
SehyeonGil <rlftpgus@naver.com>
2019-10-08 18:29:39 +0900

    before cart 2

--- routes/payment/imp_authenticate.js
+++ routes/payment/imp_authenticate.js
@@ -6,20 +6,13 @@ module.exports = async function (){
     const uri = 'https://api.iamport.kr/users/getToken'
     const IMP_API_access_key = SecretKey.IMP_API_access_key;
     const IMP_API_secret_key = SecretKey.IMP_API_secret_key;
-    const result = await request({
+    return await request({
         method: method,
         json: true,
         uri: uri,
-        headers: {
-            /*'Content-Type': 'application/json; charset=utf-8',
-            'x-ncp-iam-access-key' : NCP_accessKey,
-            'x-ncp-apigw-timestamp': timestamp,
-            'x-ncp-apigw-signature-v2': signature.toString()*/
-        },
         body: {
-            imp_key:IMP_API_access_key,
-            imp_secret:IMP_API_secret_key
+            imp_key: IMP_API_access_key,
+            imp_secret: IMP_API_secret_key
         }
     });
-    return result;
 };
